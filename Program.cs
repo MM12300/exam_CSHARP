38bae888e46c098b25732b4bde510aa59a33e7d8
Matthieu <nerggalkore@gmail.com>
2022-11-15 09:41:56 +0100

    Start

--- Program.cs
+++ Program.cs
@@ -1,9 +1,32 @@
-﻿namespace EXAM_CSHARP
+﻿using System;
+using System.IO;
+
+namespace EXAM_CSHARP
 {
     internal class Program
     {
+        public class Planet
+        {
+            public int Size { get; set; }
+            public int Usability { get; set; }
+            public int Orbit { get; set; }
+            public string Name { get; set; }
+        }
+
         public static void Main(string[] args)
         {
+            string dirPath = @"..\..\Universe";
+            //Get list of all directories within Universe folder
+            string[] dirs = Directory.GetDirectories(dirPath, "*", SearchOption.TopDirectoryOnly);
+            foreach (string dir in dirs)
+            {
+                Console.WriteLine(dir);
+                //Get list of all files within each Universe subfolders (systems)
+                string[] allfiles = Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories);
+                foreach (var file in allfiles){
+                    Console.WriteLine(file);
+                }
+            }
         }
     }
 }
