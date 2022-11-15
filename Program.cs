72d3821502537f10aa9f2cccb06c835980b6d88d
Matthieu <nerggalkore@gmail.com>
2022-11-15 10:29:00 +0100

    exercice 2 fail

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace EXAM_CSHARP
@@ -26,14 +28,19 @@ namespace EXAM_CSHARP
             public List<System> Systems { get; set; }
         }
         
+        //Main for EXERCICE 1
+        // public static void Main(string[] args)
+        // {
+        //     UniverseDeserialize();
+        // }
 
-        public static void Main(string[] args)
-        {
-            UniverseDeserialize();
-        }
-
+        // EXERCICE 1 
         public static void UniverseDeserialize()
         {
+            // Mesuring time for method execution with stopwatch (start)
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            
             // Universe folder relative path
             string dirPath = @"..\..\Universe";
 
@@ -46,12 +53,12 @@ namespace EXAM_CSHARP
                 //Initialize a new System
                 System system = new System() { Name = dir, Planets = new List<Planet>() };
                 
-                Console.WriteLine(dir);
+                //Console.WriteLine(dir);
                 //Get list of all files within each Universe subfolders (systems)
                 string[] allfiles = Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories);
                 
                 foreach (var file in allfiles){
-                    Console.WriteLine(file);
+                    // Console.WriteLine(file);
                     //Check if file exists
                     if (File.Exists(file))
                     {
@@ -63,6 +70,72 @@ namespace EXAM_CSHARP
                 // Add this system to the Universe
                 universe.Systems.Add(system);
             }
+            //Stop the stopwatch
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+        }
+
+        // Main for EXERCICE 2
+        public static async Task Main()
+        {
+            await UniverseDeserializeAsync();
+        
+        }
+
+        // EXERCICE 2
+        public static async Task UniverseDeserializeAsync()
+        {
+            // Mesuring time for method execution with stopwatch (start)
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            
+            // Universe folder relative path
+            string dirPath = @"..\..\Universe";
+
+            Universe universe = new Universe() { Systems = new List<System>() };
+            
+            //Get list of all directories within Universe folder
+            string[] dirs = Directory.GetDirectories(dirPath, "*", SearchOption.TopDirectoryOnly);
+            foreach (string dir in dirs)
+            {
+                //Initialize a new System
+                System system = new System() { Name = dir, Planets = new List<Planet>() };
+                
+                //Get list of all files within each Universe subfolders (systems)
+                string[] allfiles = Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories);
+                
+                foreach (var file in allfiles){
+                    //Check if file exists
+                    if (File.Exists(file))
+                    {
+                        string planetFile = File.ReadAllText(file);
+                        Planet planet = await Task.Run(() =>
+                        {
+                            return JsonConvert.DeserializeObject<Planet>(planetFile);
+                        });
+                        system.Planets.Add(planet);
+                    }
+                }
+                // Add this system to the Universe
+                universe.Systems.Add(system);
+            }
+            
+            //Stop the stopwatch
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
         }
     }
 }
